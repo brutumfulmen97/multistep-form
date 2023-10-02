222de244675239254af47f12c8a6b02f61a790f4
brutumfulmen97 <vlatkooop@gmail.com>
2023-10-02 21:03:20 +0200

    finito

--- src/Steps/Plan.tsx
+++ src/Steps/Plan.tsx
@@ -1,22 +1,20 @@
-import { FC, useState } from "react";
+import { useState } from "react";
 import { MdGamepad } from "react-icons/md";
 import { IoGameControllerSharp } from "react-icons/io5";
 import { FaGamepad } from "react-icons/fa";
 import { useAppState } from "../context/state";
 import { useNavigate } from "react-router-dom";
 
-interface PlanProps {}
-
-const Plan: FC<PlanProps> = () => {
+const Plan = () => {
     const [state, setState] = useAppState();
-    const [pricing, setPricing] = useState(state.pricing);
+    const [pricing, setPricing] = useState(state.pricing || "monthly");
     const [plan, setPlan] = useState(state.plan);
     const navigate = useNavigate();
 
     const saveData = () => {
         if (pricing === "monthly") {
             switch (plan) {
-                case "arcade":
+                case "Arcade":
                     setState({
                         ...state,
                         price: 6,
@@ -24,7 +22,7 @@ const Plan: FC<PlanProps> = () => {
                         plan: "Arcade",
                     });
                     break;
-                case "advanced":
+                case "Advanced":
                     setState({
                         ...state,
                         price: 9,
@@ -32,7 +30,7 @@ const Plan: FC<PlanProps> = () => {
                         plan: "Advanced",
                     });
                     break;
-                case "pro":
+                case "Pro":
                     setState({
                         ...state,
                         price: 12,
@@ -43,7 +41,7 @@ const Plan: FC<PlanProps> = () => {
             }
         } else {
             switch (plan) {
-                case "arcade":
+                case "Arcade":
                     setState({
                         ...state,
                         price: 60,
@@ -51,7 +49,7 @@ const Plan: FC<PlanProps> = () => {
                         plan: "Arcade",
                     });
                     break;
-                case "advanced":
+                case "Advanced":
                     setState({
                         ...state,
                         price: 90,
@@ -59,7 +57,7 @@ const Plan: FC<PlanProps> = () => {
                         plan: "Advanced",
                     });
                     break;
-                case "pro":
+                case "Pro":
                     setState({
                         ...state,
                         price: 120,
@@ -158,13 +156,13 @@ const Plan: FC<PlanProps> = () => {
                 <p>Monthly</p>
                 <input
                     onChange={(e) => {
-                        if (e.target.checked) setPricing("yearly");
+                        if (e.target.checked === true) setPricing("yearly");
                         else setPricing("monthly");
                     }}
                     className="mr-2 mt-[0.3rem] h-3.5 w-8 appearance-none rounded-[0.4375rem] bg-neutral-300 before:pointer-events-none before:absolute before:h-3.5 before:w-3.5 before:rounded-full before:bg-transparent before:content-[''] after:absolute after:z-[2] after:-mt-[0.1875rem] after:h-5 after:w-5 after:rounded-full after:border-none after:bg-neutral-100 after:shadow-[0_0px_3px_0_rgb(0_0_0_/_7%),_0_2px_2px_0_rgb(0_0_0_/_4%)] after:transition-[background-color_0.2s,transform_0.2s] after:content-[''] checked:bg-primary checked:after:absolute checked:after:z-[2] checked:after:-mt-[3px] checked:after:ml-[1.0625rem] checked:after:h-5 checked:after:w-5 checked:after:rounded-full checked:after:border-none checked:after:bg-primary checked:after:transition-[background-color_0.2s,transform_0.2s] checked:after:content-[''] hover:cursor-pointer focus:outline-none focus:ring-0 focus:before:scale-100 focus:before:opacity-[0.12]  focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-5 focus:after:w-5 focus:after:rounded-full focus:after:content-[''] checked:focus:border-primary checked:focus:bg-primary checked:focus:before:ml-[1.0625rem]   dark:bg-neutral-600 dark:after:bg-blue-400 dark:checked:bg-primary dark:checked:after:bg-primary   disable:opacity-30 disabled:cursor-not-allowed"
                     type="checkbox"
                     role="switch"
-                    checked={state.pricing === "yearly"}
+                    checked={pricing === "yearly"}
                     id="flexSwitchCheckDefault"
                 />
                 <p>Yearly</p>
@@ -177,7 +175,7 @@ const Plan: FC<PlanProps> = () => {
                     Go back
                 </button>
                 <button
-                    onClick={saveData}
+                    onClick={() => saveData()}
                     className="block w-24 text-sm bg-blue-900 text-white py-2 rounded-md"
                 >
                     Next Step
